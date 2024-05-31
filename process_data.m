4e7b94014eee2f1b7d459b3152f733ad985fa463
Alexandros Rosakis <ayrosaki@caltech.edu>
2024-05-31 10:14:05 -0700

    added get start of pulse function

--- process_data.m
+++ process_data.m
@@ -8,14 +8,27 @@ clc
 data_path = 'data_05_26_2024_16_54_50/';
 load([data_path,'acquisition_params.mat']);
 
-% get key parameters
+% get acquisition parameters
 n_acquisition_cycles             = acquisition_params.n_acquisition_cycles;
 n_samples_per_acquisition_cycle  = acquisition_params.n_samples_per_acquisition_cycle;
 n_triggers_per_acquisition_cycle = acquisition_params.n_triggers_per_acquisition_cycle;
 
+% instantiate PostProcessingParameters object
+post_processing_params = PostProcessingParameters();
+
+% get key parameters
+mask_threshold      = post_processing_params.mask_threshold;
+image_crop_array    = post_processing_params.image_crop_array;
+smooth_before_fit   = post_processing_params.smooth_before_fit;
+spike_detect_thresh = post_processing_params.spike_detect_thresh;
+noise_floor         = post_processing_params.noise_floor;
+zero_length         = post_processing_params.zero_length;
+
+
 % get derived parameters
-n_waveforms            = n_acquisition_cycles*n_triggers_per_acquisition_cycle;
-n_samples_per_waveform = n_samples_per_acquisition_cycle/n_triggers_per_acquisition_cycle;
+n_valid_triggers_per_acquisition_cycle = n_triggers_per_acquisition_cycle - 1; 
+n_waveforms                            = n_acquisition_cycles * n_valid_triggers_per_acquisition_cycle;
+n_samples_per_waveform                 = n_samples_per_acquisition_cycle/n_triggers_per_acquisition_cycle;
 
 % define the data frame 
 X = zeros(n_waveforms,n_samples_per_waveform);
@@ -27,34 +40,29 @@ for i = 1:n_acquisition_cycles
     load([data_path,'pulse_',num2str(i),'.mat']);
     
     % get start and end indices
-    start_idx = 1 + (i-1)*n_triggers_per_acquisition_cycle;
-    end_idx   = i*n_triggers_per_acquisition_cycle;
+    start_idx = 1 + (i-1)*n_valid_triggers_per_acquisition_cycle;
+    end_idx   = i*n_valid_triggers_per_acquisition_cycle;
     
     % put reshaped data into data frame
-    X(start_idx:end_idx,:) = reshape(w,n_samples_per_waveform,n_triggers_per_acquisition_cycle)';
+    start_pos = get_start_of_pulse(w,n_samples_per_waveform,spike_detect_thresh,noise_floor,zero_length);
+  
+    w_phase_locked = w(start_pos:end -(n_samples_per_waveform-start_pos)-1);
+    X(start_idx:end_idx,:) = reshape(w_phase_locked,n_samples_per_waveform,n_valid_triggers_per_acquisition_cycle)';
 end
 
 % save waveform data 
-save([data_path,'X.mat'],'X');
+% save([data_path,'X.mat'],'X');
 
-% instantiate PostProcessingParameters object
-post_processing_params = PostProcessingParameters();
-
-% get key parameters
-mask_threshold    = post_processing_params.mask_threshold;
-image_crop_array  = post_processing_params.image_crop_array;
-smooth_before_fit = post_processing_params.smooth_before_fit;
-
-% get the curvature and position arrays
-[curvature_array,x_array,y_array] = getCurvatureAndPositionArrays(data_path,mask_threshold,image_crop_array,smooth_before_fit);
-
-% curvature array and position arrays
-save([data_path,'Y.mat'],'curvature_array');
-save([data_path,'px_array.mat'],'x_array');
-save([data_path,'py_array.mat'],'y_array');
+% % get the curvature and position arrays
+% [curvature_array,x_array,y_array] = getCurvatureAndPositionArrays(data_path,mask_threshold,image_crop_array,smooth_before_fit);
+% 
+% % curvature array and position arrays
+% save([data_path,'Y.mat'],'curvature_array');
+% save([data_path,'px_array.mat'],'x_array');
+% save([data_path,'py_array.mat'],'y_array');
 
 % save post_processing parameters
-save([fname,'/post_processing_params.mat'],'post_processing_params');
+% save([fname,'/post_processing_params.mat'],'post_processing_params');
 
 % plot if needed to verify the fit
 % plotFitCurvatureImage(curvature_array,x_array,y_array)
--- test.m
+++
@@ -1,2 +0,0 @@
-%testing some stuff
-
