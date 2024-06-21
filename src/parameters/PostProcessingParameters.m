fbc6e0fd03eb4c0799a8310d3ce0cc23a227679c
Alexandros Rosakis <ayrosaki@caltech.edu>
2024-06-21 16:02:04 -0700

    fixed processing file to save correctly to data folder

--- src/parameters/PostProcessingParameters.m
+++ src/parameters/PostProcessingParameters.m
@@ -6,8 +6,8 @@ classdef PostProcessingParameters
         smooth_before_fit                 = false;
         
         %phase delay parameters 
-        spike_detect_thresh               = -0.45;
-        noise_floor                       = 0.02
+        spike_detect_thresh               = -2.45;
+        noise_floor                       = 0.1
         zero_length                       = 1000;
     end
 end
