2bbb8e8c45480d06b3459599c753d85d0afe3bee
Alexandros Rosakis <ayrosaki@caltech.edu>
2024-12-11 17:04:44 -0800

    test nn 3d now works

--- 3D/test_nn_3D.m
+++ 3D/test_nn_3D.m
@@ -8,7 +8,7 @@ net = importONNXNetwork('C:\Users\arosa\Box\USS Catheter\3d_data\data_11_08_2024
 
 %% load
 load('C:\Users\arosa\Box\USS Catheter\3d_data\data_11_08_2024_16_47_09\X.mat');
-load('C:\Users\arosa\Box\USS Catheter\3d_data\data_11_08_2024_16_47_09\position_mat_tot.amt');
+load('C:\Users\arosa\Box\USS Catheter\3d_data\data_11_08_2024_16_47_09\position_matrix_tot.mat');
 load('C:\Users\arosa\Box\USS Catheter\3d_data\data_11_08_2024_16_47_09\Theta_relative_3D.mat');
 load('C:\Users\arosa\Box\USS Catheter\3d_data\data_11_08_2024_16_47_09\acquisition_params.mat')
 
@@ -24,11 +24,11 @@ dt = t(2)-t(1);
 %% get normalized input data
 
 % remove nans from theta and also remove from same indices in X
-Theta_relative_cleaned = Theta_relative_3d(~any(isnan(Theta_relative_3d), 2), :);
-X_cleaned              = X(~any(isnan(Theta_relative_3d), 2), :);
+Theta_relative_cleaned = Theta_relative_3D(~any(isnan(Theta_relative_3D), 2), :);
+X_cleaned              = X(~any(isnan(Theta_relative_3D), 2), :);
 
 % assign back to values
-Theta_relative_3d = Theta_relative_cleaned;
+Theta_relative_3D = Theta_relative_cleaned;
 X = X_cleaned;
 
 % Normalize the input data X
@@ -37,16 +37,16 @@ X_std = std(X, 1);    % Compute standard deviation along the first dimension (ro
 X_normalized = (X - X_mean) ./ (X_std + 1e-6);
 
 % Normalize the output data Theta
-Theta_relative_mean = mean(Theta_relative_3d, 1);  % Compute mean along the first dimension (rows)
-Theta_relative_std = std(Theta_relative_3d, 1);    % Compute standard deviation along the first dimension (rows)
-Theta_relative_normalized = (Theta_relative_3d - Theta_relative_mean) ./ (Theta_relative_std + 1e-6);
+Theta_relative_mean = mean(Theta_relative_3D, 1);  % Compute mean along the first dimension (rows)
+Theta_relative_std = std(Theta_relative_3D, 1);    % Compute standard deviation along the first dimension (rows)
+Theta_relative_normalized = (Theta_relative_3D - Theta_relative_mean) ./ (Theta_relative_std + 1e-6);
 
 
 %% get wire length in pixels (MODIFY)
 L = 0.5; %this is arbitrary 
 
 %% get wire clamped position in y
-py_clamp = Py(1,1);
+% py_clamp = Py(1,1);
 
 %% number of joints on kinematic linkage
 n_joints = 5; %there are actually 2x this many but each is a double joint (one around xy, one around xz)
@@ -67,12 +67,12 @@ filter = IdentityFilter();
 % visualization
 point_num = 1; %which point is the zero position
 filtering       = true;
-Theta_predicted = zeros(size(Theta_relative));
+Theta_predicted = zeros(size(Theta_relative_3D));
 figure();
 hold on;
-for i = 1:size(Px,1)
+for i = 1:length(Theta_relative_3D(:,1))
     % get current starting position (MODIFY)
-    py_clamp = Py(i,1);
+%     py_clamp = Py(i,1);
 
     % Convert input data to a dlarray
     dlInput = dlarray(X_normalized(i,nx_start:nx_end), 'BC');
@@ -89,30 +89,34 @@ for i = 1:size(Px,1)
     end          
 
     % plot kinematic linkage with given angles (MODIFY)
-    max_x = max(Px(i,:));
-    plot(Px(i,1:max_x),Py(i,1:max_x),"Color",'b',LineWidth=1.0);
-    axis([0,L,0,1080])
-    hold on
+%     max_x = max(Px(i,:));
+%     plot(Px(i,1:max_x),Py(i,1:max_x),"Color",'b',LineWidth=1.0);
+%     axis([0,L,0,1080])
+%     hold on
 
     % at update rate plot the model prediction
     if mod(i,1) == 0
+        
         % get and plot the predicted shape
         subplot(2,1,1)
         scatter(Position_matrix_tot(1,:,i)-Position_matrix_tot(1,point_num,i),Position_matrix_tot(2,:,i)-Position_matrix_tot(2,point_num,i))
         axis([-0.3,0.7,-.3,0.3])
         hold on
-        Pkin = forward_kin5(a,Theta_relative_yx(i,:));
+        Pkin = forward_kin5(a,Theta_relative_3D(i,1:5));
         plot(Pkin(2,:)-Pkin(2,1),Pkin(1,:)-Pkin(1,1),'Marker','o','MarkerFaceColor','k',"Color",'r','MarkerEdgeColor','k',LineWidth=1.0);
-        
+        Pkin = forward_kin5(a,Theta_relative_filtered(1:5)');
+        plot(Pkin(2,:)-Pkin(2,1),Pkin(1,:)-Pkin(1,1),'Marker','o','MarkerFaceColor','k',"Color",'b','MarkerEdgeColor','k',LineWidth=1.0);        
         
         
         subplot(2,1,2)
         scatter(Position_matrix_tot(1,:,i)-Position_matrix_tot(1,point_num,i),Position_matrix_tot(3,:,i)-Position_matrix_tot(3,point_num,i))
         axis([-0.3,0.7,-.3,0.3])
         hold on
-        Pkin = forward_kin5(a,Theta_relative_zx(i,:));
+        Pkin = forward_kin5(a,Theta_relative_3D(i,6:10));
         plot(Pkin(2,:)-Pkin(2,1),Pkin(1,:)-Pkin(1,1),'Marker','o','MarkerFaceColor','k',"Color",'r','MarkerEdgeColor','k',LineWidth=1.0);
-        
+        Pkin = forward_kin5(a,Theta_relative_filtered(6:10)');
+        plot(Pkin(2,:)-Pkin(2,1),Pkin(1,:)-Pkin(1,1),'Marker','o','MarkerFaceColor','k',"Color",'b','MarkerEdgeColor','k',LineWidth=1.0);
+
 
     
     end
