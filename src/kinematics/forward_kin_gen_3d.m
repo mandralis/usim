4ffdba59e54e1fa65ab977f459461b833f8d13e5
Ioannis Mandralis <imandralis@gmail.com>
2024-12-18 11:28:36 -0800

    added 3d forward kinematics code

--- src/kinematics/forward_kin_gen_3d.m
+++ src/kinematics/forward_kin_gen_3d.m
@@ -3,26 +3,42 @@ close all
 clc
 
 % define number of joints of kinematic linkage
-n_joints = 5; 
-t = sym('t',[1,n_joints]);
+n_joints = 5;
+n_per_joint = 2;
+t = sym('t',[1,n_joints*n_per_joint]);
 a = sym('a',[1,n_joints+1]);
 
 %initial conditions for each joint in the home position
 theta = t;
-q     = sym(zeros(3,n_joints+1));
-q(:,1) = [0;a(1);0];
+q_first     = sym(zeros(3,n_joints+1));
+q_second  = sym(zeros(3,n_joints+1));
+
+q_first(:,1) = [0;a(1);0];
+q_second(:,1) = [0;a(1);0];
+
 for i=1:n_joints
-    q(:,i+1) = q(:,i) + [0;a(i+1);0];
+    q_first(:,i+1) = q_first(:,i) + [0;a(i+1);0];
+    q_second(:,i+1) = q_first(:,i) + [0;a(i+1);0];
 end
 
+j =1 ;
+for i = 1:2:n_joints*n_per_joint+1 
+
+    q(:,i) = q_first(:,j);
+    q(:,i+1) = q_second(:,j);
+    j = j+1;
+end
+%%
 %rotation axes of the joints in the home position 
-w = zeros(3,n_joints);
-for i=1:n_joints
+w = zeros(3,n_joints*n_per_joint);
+for i=1:2:n_joints*n_per_joint
     w(:,i) = [0;0;1];
+    w(:,i+1) = [1;0;0];
+
 end
 
 %find initial twist matrices 
-for i = 1:n_joints
+for i = 1:n_joints*n_per_joint
     v = cross(-w(:,i),q(:,i));
     g(:,:,i) = twist(w(:,i),v,theta(i));    
 end
@@ -37,7 +53,7 @@ gpre = eye(4);
 %also need to initialize the first position to be indicative of q1 pos
 
 pos(:,1) = q(:,1);
-for i = 1:n_joints
+for i = 1:n_joints*n_per_joint
     gend(1:3,4) = q(:,i+1);
     gend(4,1:4) = [0 0 0 1];
     gend(1:3,1:3) = [1 0 0;0 1 0;0 0 1];
@@ -46,8 +62,11 @@ for i = 1:n_joints
     pos(:,i+1) = gst(1:3,4,i);
 end
 
+% position for plot
+pos_ = pos(:,1:2:end);
+
 % make a matlab function for later use
-forward_kinematics = matlabFunction(pos,"File","src/kinematics/forward_kin5","Vars",{a,t});
+forward_kinematics = matlabFunction(pos_,"File","src/kinematics/forward_kin_3d","Vars",{a,t});
 
 %function used to calculate twist matrices
 function mat = twist(w,v,theta)
